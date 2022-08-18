16eccf885b8d60a435fe8bb9735e86cdf810d5b6
David Qian <david.qian02@gmail.com>
2022-08-18 00:20:44 -0400

    Update python files

--- Client.py
+++ Client.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
 import requests
 import base64
 import datetime
 from urllib.parse import urlencode
+from IPython.display import Image, display
 
 
+
+# search, recommendByTrack, getAudioFeatures, getRelatedArtists
 class SpotifyAPI():
     access_token = None
     expires = datetime.datetime.now()
-    expired = True
     client_id = None
     client_secret = None
     redirect_uri = 'http://localhost:8888/notebooks/Autho.ipynb'
     token_url = 'https://accounts.spotify.com/api/token'
-    
+    headers = None
     
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
     
-    def __getTokenData(self):
+    def getTokenData(self):
         return {
             'grant_type': 'client_credentials'
         }
-    def __getTokenHeaders(self):
+    def getTokenHeaders(self):
         client_creds = f"{self.client_id}:{self. client_secret}"
         client_creds_b64 = base64.b64encode(client_creds.encode())
         return {
@@ -40,7 +40,107 @@ class SpotifyAPI():
         self.access_token = response['access_token']
         expires_in = response['expires_in']
         self.expires = now + datetime.timedelta(seconds=expires_in)
-        self.expired = expires < now
         return True
-        
 
+    def getToken(self):
+        if self.expires < datetime.datetime.now():
+            self.performAuth()
+        self.headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    def displaySearch(self, result, searchtype, getID):
+        if searchtype == 'track':
+            # 0 index meaning first result
+            title = result['tracks']['items'][0]['name']
+            artist = result['tracks']['items'][0]['artists'][0]['name']
+            artist_id = result['tracks']['items'][0]['artists'][0]['id']
+            song_id = result['tracks']['items'][0]['id']
+            if getID:
+                return song_id, artist_id
+            link = f"https://open.spotify.com/track/{song_id}"
+            artist_page = f"https://open.spotify.com/artist/{artist_id}"
+            img = result['tracks']['items'][0]['album']['images'][1]['url']
+            release_date = result['tracks']['items'][0]['album']['release_date']
+            display(Image(url=img))
+            print(f"Title: {title}\nArtist: {artist}\nRelease Date: {release_date}\nLink: {link}\nArtist Profile: {artist_page}")
+        elif searchtype == 'artist':
+            if getID:
+                return result['artists']['items'][0]['id']
+            artist = result['artists']['items'][0]['name']
+            link = result['artists']['items'][0]['external_urls']['spotify']
+            followers = result['artists']['items'][0]['followers']['total']
+            img = result['artists']['items'][0]['images'][1]['url']
+            display(Image(url=img))
+            print(f"Name: {artist}\nFollower Count: {followers}\nProfile: {link}")
+        elif searchtype == 'album':
+            img = result['albums']['items'][0]['images'][1]['url']
+            display(Image(url=img))
+            # TODO: Display Album Info
+            
+        else:
+            print("Choose type: track, artist, or album")
+        return result
+    
+    
+    # User Functions Below
+    
+    # search track, artist, album
+    def search(self, name, searchtype, getID = False):
+        self.getToken()
+        data = {'q': name, 'type': searchtype.lower()}
+        dataurl = urlencode(data)
+        url = f"https://api.spotify.com/v1/search?{dataurl}"
+        r = requests.get(url, headers=self.headers)
+        result = r.json()
+        
+        return self.displaySearch(result, searchtype, getID)
+    
+    # input one or two songs
+    def recommendByTrack(self, song1, song2=''):
+        self.getToken()
+        song1_id, artist1_id = self.search(song1, 'track', getID=True)
+        data = {'seed_artists': artist1_id, 'seed_tracks': song1_id}
+        if song2 != '':
+            song2_id, artist2_id = self.search(song2, 'track', getID=True)
+            if artist2_id != artist1_id:
+                data['seed_artists'] += f',{artist2_id}'
+            data['seed_tracks'] += f',{song2_id}'
+            
+        dataurl = urlencode(data)
+        url = f"https://api.spotify.com/v1/recommendations?{dataurl}"
+        r = requests.get(url, headers=self.headers)
+        response = r.json()
+        
+        for i in range(10):
+            name = response['tracks'][i]['album']['artists'][0]['name']
+            album = response['tracks'][i]['album']['name']
+            link = response['tracks'][i]['album']['external_urls']['spotify']
+            img = response['tracks'][i]['album']['images'][1]['url']
+            display(Image(url=img))
+            print(f"{i+1}. Artist: {name}\nAlbum: {album}\nSpotify Link: {link}\n")
+        return response
+    
+    # search by artist or track using isArtist bool
+    def getRelatedArtists(self, name, isArtist):
+        if isArtist:
+            _id = self.search(name, 'artist', getID=True)
+        else:
+            _id = self.search(name, 'track', getID=True)[1]
+        url = f'https://api.spotify.com/v1/artists/{_id}/related-artists'
+        r = requests.get(url, headers = self.headers)
+        response = r.json()
+        print("Similar Artists:")
+        for i in range(20):
+            print(i+1, response['artists'][i]['name'])
+        return response
+    
+    def getAudioFeatures(self, track):
+        _id = self.search(track, 'track', getID=True)[0]
+        url = f'https://api.spotify.com/v1/audio-features/{_id}'
+        r = requests.get(url, headers=self.headers)
+        # TODO: Parse / visualize audio features (e.g. danceability, energy)
+        
+        return r.json()
+    
--- spotifyprj.py
+++ spotifyprj.py
@@ -1,70 +1,11 @@
-import requests
-import base64
+from Client import SpotifyAPI
 
 CLIENT_ID = '4e75cab0012d409da502214e828ade40'
 CLIENT_SECRET = '3a5f1a1c83f442f991b1321a4e78edac'
-client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
-client_creds_b64 = base64.b64encode(client_creds.encode())
-
-user_id = 'panda6722'
-artistURL = f'https://api.spotify.com/v1/me/top/artists'
-trackURL = 'https://api.spotify.com/v1/me/top/tracks'
-playlistURL = f'https://api.spotify.com/v1/users/{user_id}/playlists'
-
-auth_url = 'https://accounts.spotify.com/authorize'
-auth_code = requests.get(auth_url, {
-    'client_id': CLIENT_ID,
-    'response_type': 'code',
-    'redirect_uri': 'http://localhost:8888/notebooks/Autho.ipynb',
-})
-token_url = 'https://accounts.spotify.com/api/token'
-token_headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'authorization': f"Basic {client_creds_b64.decode()}" # <base64 encoded client_id:client_secret>
-}
-auth_data = {
-    'grant_type': 'authorization_code',
-    'code': auth_code,
-    'redirect_uri': 'http://localhost:8888/notebooks/Autho.ipynb',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-}
-auth_r = requests.post(token_url, data = auth_data, headers = token_headers)
-print(auth_r.json())
-ACCESS_TOKEN = auth_r.json()["access_token"]
-
-headers = {
-    "Authorization": f"Bearer {ACCESS_TOKEN}", 
-    'Content-Type': 'application/json',
-    "Accept": "application/json"
-}
-
-def createPlaylist():
-    response = requests.get(playlistURL, headers=headers, json={'name':'Test'})
-    myjson = response.json()
-    print(myjson)
-
-def getTopArtists():
-    response = requests.get(artistURL, headers=headers, 
-                            json={"limit":10, 'time_range': 'short_term'})
-
-    myjson = response.json()
-    print(myjson)
-    return myjson
-
-def getTopTracks():
-    response = requests.post(trackURL, headers=headers, 
-                            json={"limit":10, 'time_range': 'short_term'})
-
-    myjson = response.json()
-    return myjson
 
 def main():
-    # artists = getTopArtists()
-    # tracks = getTopTracks()
-    # print(artists)
-    # print(tracks)
-    createPlaylist()
+    client = SpotifyAPI(CLIENT_ID, CLIENT_SECRET)
+    res = client.recommendByTrack("Glimpse of us", 'dorothea')
 
 if __name__ == "__main__":
     main()
